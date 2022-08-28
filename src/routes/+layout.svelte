81757a17f39ba6756cfd390da02356fac9fa4abf
webdevKmer <webdevKmer@gmail.com>
2022-08-28 18:03:54 +0000

    devolop html structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/todos">Todos</a>
+</nav>
+<hr>
+
+<slot></slot>
+
+
+<hr>
+<footer>
+    <h5>Made with heart by web dev Kmer.</h5>
+</footer>
---
+++ src/routes/todos/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import TodoItem from "../components/todo-item.svelte";
+</script>
+
+<svelte:head>
+    <title>Web App - Todos</title>
+</svelte:head>
+
+<h1>Todos App</h1>
+
+<form method="post">
+    <input type="text" name="todo" aria-label="Add a todo" placeholder="Add Todo ...">
+    <input type="submit" value="Add New Todo">
+</form>
+
+<TodoItem />
